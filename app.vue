aa6aa428d245665680a1f077856fc290f097aa88
Mustafa Peynirci <mustafapeynircigithub@gmail.com>
2023-07-20 21:58:43 +0300

    arrangement

--- app.vue
+++ app.vue
@@ -299,7 +299,7 @@
 </template>
 <script setup>
 useHead({
-  title: 'My App',
+  title: 'Teknokoz | Yazılım',
   link: [
     {
       href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
